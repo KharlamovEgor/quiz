20d74f632f76d485037aaf00b2f0958e8e6f0166
Kharlamov Egor <kharlamovegormain@mail.ru>
2024-09-12 18:41:20 +0300

    Diagram is added to results

---
+++ src/components/diagram/Diagram.tsx
@@ -0,0 +1,70 @@
+import styles from "./Diagram.module.css";
+import Chart from "chart.js/auto";
+import classNames from "classnames";
+import { useLayoutEffect, useRef } from "react";
+interface Props {
+  blocks: string[];
+  scores: number[];
+  className?: string;
+}
+export function Diagram({ blocks, scores, className }: Props) {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useLayoutEffect(() => {
+    const myChart = new Chart(ref.current!, {
+      type: "polarArea",
+      options: {
+        responsive: true,
+        scales: {
+          r: {
+            ticks: {
+              display: false,
+            },
+            pointLabels: {
+              display: false,
+            },
+            angleLines: {
+              display: true,
+              color: "#fff",
+              lineWidth: 2,
+            },
+            grid: {
+              color: "#fff",
+              lineWidth: 2,
+            },
+            startAngle: -50,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+      data: {
+        labels: blocks.map((b) => b.slice(9)),
+        datasets: [
+          {
+            data: scores,
+            borderColor: "#fff",
+            backgroundColor: [
+              "#DE9292",
+              "#C692DE",
+              "#929EDE",
+              "#AADE92",
+              "#DECE92",
+            ],
+          },
+        ],
+      },
+    });
+
+    return () => {
+      myChart.destroy();
+    };
+  }, []);
+
+  return (
+    <canvas className={classNames(styles.canvas, className)} ref={ref}></canvas>
+  );
+}
